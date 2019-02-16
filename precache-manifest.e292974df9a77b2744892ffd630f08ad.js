e5ea28ecc3eb68a5ea493c26a9a963b0b8044f81
liuJJJJJ <1058350816@qq.com>
2019-02-16 18:09:50 +0800

    Updates

---
+++ precache-manifest.e292974df9a77b2744892ffd630f08ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30ce0204bf44206d21eceae7aacd419a",
+    "url": "/tairanjinrong/static/media/ui-box-bg.30ce0204.30ce0204.png"
+  },
+  {
+    "revision": "d8986857e94c852b43b64318ae650c69",
+    "url": "/tairanjinrong/static/media/banner3.d8986857.png"
+  },
+  {
+    "revision": "17bad82ef3529110f080524b087ce9c0",
+    "url": "/tairanjinrong/static/media/banner2.17bad82e.png"
+  },
+  {
+    "revision": "82ec643ccebcfe4d1da786788d16b88d",
+    "url": "/tairanjinrong/static/media/banner1.82ec643c.png"
+  },
+  {
+    "revision": "b12589eadd7979e0162d",
+    "url": "/tairanjinrong/static/js/runtime~main.b12589ea.js"
+  },
+  {
+    "revision": "26c758c77405313e689e",
+    "url": "/tairanjinrong/static/js/main.26c758c7.chunk.js"
+  },
+  {
+    "revision": "dbd4faee7ee04e32130d",
+    "url": "/tairanjinrong/static/js/1.dbd4faee.chunk.js"
+  },
+  {
+    "revision": "26c758c77405313e689e",
+    "url": "/tairanjinrong/static/css/main.386fa6e7.chunk.css"
+  },
+  {
+    "revision": "3513c0bde359431f44c573aa76e39cad",
+    "url": "/tairanjinrong/index.html"
+  }
+];
